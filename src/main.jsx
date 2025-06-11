47bcf30b77587465cebc4f1b1632eb32d820a17b
IsmaelPrado <vansestilo200@gmail.com>
2025-06-11 17:49:50 -0600

    Se añaden archivos docker para producción con nginxy para local

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const basename = process.env.NODE_ENV === 'production' ? '/Desafio03Docker' : '/';
+const basename =  '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
